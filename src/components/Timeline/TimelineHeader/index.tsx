8181cf3809ff0803e65bcff343bd3f9d8fc8f255
Zain <zain@this.so>
2023-04-27 00:35:11 +0500

    Remove header shadow

--- src/components/Timeline/TimelineHeader/index.tsx
+++ src/components/Timeline/TimelineHeader/index.tsx
@@ -245,13 +245,6 @@ export default TimelineHeader;
 
 const styles = StyleSheet.create({
   container: {
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
     elevation: 3,
     zIndex: 99,
   },
