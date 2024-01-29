94eeb47a8aa28e59e35bed951d2acfdc187447e9
yzzing <85097131+yzzing@users.noreply.github.com>
2024-01-29 16:51:09 +0900

    Update DragCreateItem.tsx

--- src/components/Timeline/DragCreateItem.tsx
+++ src/components/Timeline/DragCreateItem.tsx
@@ -33,7 +33,7 @@ const DragCreateItem = ({
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      height: (dragCreateInterval / 60) * heightByTimeInterval.value,
+      height: (dragCreateInterval / 60) * heightByTimeInterval.value - 1, // 변경
       transform: [{ translateX: offsetX.value }, { translateY: offsetY.value }],
     };
   });
@@ -46,7 +46,7 @@ const DragCreateItem = ({
           {
             left: hourWidth,
             backgroundColor: theme.dragCreateItemBackgroundColor,
-            width: columnWidth,
+            width: columnWidth - 1, // 변경 
           },
           animatedStyles,
         ]}
@@ -146,6 +146,7 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     top: 0,
     left: 0,
+    marginTop: 1,   //변경
   },
   hourContainer: {
     position: 'absolute',
